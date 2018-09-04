6fc2a52e6652299ab5fe7e0c3d5e66ca61e7b129
Jason Carr <jasonhcarr@gmail.com>
2018-09-03 21:36:26 -0400

    pagination in place and working

--- src/app/components/launch-footer/launch-footer.component.ts
+++ src/app/components/launch-footer/launch-footer.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-launch-footer',
-  templateUrl: './launch-footer.component.html',
-  styleUrls: ['./launch-footer.component.css']
+    selector: 'app-launch-footer',
+    templateUrl: './launch-footer.component.html',
+    styleUrls: ['./launch-footer.component.css']
 })
 export class LaunchFooterComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    /* The total number of pages as sent from launch-table */
+    @Input() numOfPages: number;
 
+    @Output() pageEvent = new EventEmitter<number>();
+
+    currentPage: number = 1;
+    pageRange: number[];
+
+    /**
+     * Sets the range of page buttons to display.
+     */
+    adjustPageRange = () => {
+        if ((this.numOfPages - this.currentPage) >= 4) {
+            this.pageRange = Array(5).fill(1)
+                .map((_num, i) => i + this.currentPage);
+        }
+    };
+
+    /**
+     * Decreases the currentPage value by one and updates the pageRange.
+     */
+    prevPage = () => {
+        if (this.currentPage !== 1) {
+            this.setPage(--this.currentPage);
+        }
+    };
+
+    /**
+     * Advances the currentPage value by one and updates the pageRange.
+     */
+    nextPage = () => {
+        if (this.currentPage !== this.numOfPages) {
+            this.setPage(++this.currentPage);
+        }
+    };
+
+    /**
+     * Sets the currentPage property to the value passed
+     * @param {number} pageNumber
+     */
+    setPage = (pageNumber: number) => {
+        this.currentPage = pageNumber;
+        this.adjustPageRange();
+        this.pageEvent.emit(this.currentPage - 1);
+    }
+
+    /**
+     * Sets the initial pageRange.
+     */
+    ngOnInit () {
+        this.adjustPageRange();
+    }
 }
--- src/app/components/launch-table/launch-table.component.ts
+++ src/app/components/launch-table/launch-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LaunchService } from '../../services/launch.service';
 import { ILaunch } from '../../../interfaces/launch';
+
 @Component({
     selector: 'app-launch-table',
     templateUrl: './launch-table.component.html',
@@ -13,35 +14,49 @@ export class LaunchTableComponent implements OnInit {
     /* Boolean flag for sorting purposes */
     ascending: boolean = true;
 
-    /* Static list of data returned by the launch service.
+    /* 
+     * Static list of data returned by the launch service.
      * By default, this list is ordered from oldest to most recent launch.
      */
     allLaunches: ILaunch[];
 
     /* Sorted and/or filtered list of launches */
-    displayLaunches: ILaunch[];
+    displayLaunches: ILaunch[][] = [];
 
+    rowsPerPage: number = 10;
+
+    currentIndex: number = 0;
+
+    receiveIndex = ($event: number) => {
+        this.currentIndex = $event;
+    }
     /**
      * Sorts the launch array based on the passed property value.
-     * 'allLaunches' is sorted in ascending order by default.
-     * For performance reasons, rather than resorting it can be the default when the ascending list is requested.
-     * If this were not the case, the 'sort' callback could written:
-     * @example
-     * (a, b) => {
-     *      if (this.ascending) return a[field] - b[field];
-     *      else return b[field] - a[field];
-     * }
      * @param {string} field the key on which to sort
      */
     sortTable = (field: string) => {
-        this.ascending = !this.ascending;
-        /* 
-         */
-        this.displayLaunches = this.ascending
-            ? this.allLaunches
-            // spread operator prevents object mutation
-            : [...this.allLaunches].sort(
-                (a, b) => b[field] - a[field]);
+        const ascending = this.ascending = !this.ascending;
+        this.allLaunches.sort(
+            (a, b) => {
+                if (this.ascending) return a[field] - b[field];
+                else return b[field] - a[field];
+            });
+        this.paginate();
+    };
+
+    /**
+     * Converts the flat allLaunches array into an array of arrays.
+     * The length of the internal arrays is defined by the rowsPerPage property.
+     */
+    paginate = () => {
+        const { displayLaunches, allLaunches, rowsPerPage } = this;
+        for (var i = 0; i < allLaunches.length; i++) {
+            if (i % rowsPerPage === 0) {
+                displayLaunches[Math.floor(i / rowsPerPage)] = [allLaunches[i]];
+            } else {
+                displayLaunches[Math.floor(i / rowsPerPage)].push(allLaunches[i]);
+            }
+        }
     };
 
     /**
@@ -49,8 +64,9 @@ export class LaunchTableComponent implements OnInit {
      */
     ngOnInit () {
         this.launchService.getLaunches()
-            .subscribe((data: ILaunch[]) =>
-                this.allLaunches = this.displayLaunches = data)
+            .subscribe((data: ILaunch[]) => {
+                this.allLaunches = data;
+                this.paginate();
+            });
     }
-
 }
